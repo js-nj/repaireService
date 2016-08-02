b84fdc5cffa4a6538e6591802b3383f5cc841090
fangxingxing <fangxingxing@c3bab2b4-788e-47b5-b547-fdddc749a39c>
2016-08-02 13:02:46 +0000

    post改get
    git-svn-id: svn://172.16.1.5:6789/fe/svn_server_devfe/mobile-app/repairService@1449 c3bab2b4-788e-47b5-b547-fdddc749a39c

--- src/App.vue
+++ src/App.vue
@@ -68,9 +68,7 @@ function loadView(el) {
 export default {
   created() {
     var self = this;
-    this.$http.post(global.HOST+"/ggfw/sys/itservicecommon/api/getUserLimit.do", {
-      appName: 'hqwxxt'
-    }).then(function(data) {
+    this.$http.get(global.HOST+"/ggfw/sys/itservicecommon/api/getUserLimit.do?appName=hqwxxt").then(function(data) {
       if(data.data[0] && data.data[0].GNBS == "teacher_student") {
         this.isstudent = true;
         this.headertitle = '我的报修';
