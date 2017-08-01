3cfcf7dc61ee8199e1884f73d7566b610d77b89b
xujiabin <xujiabin0114@163.com>
2017-08-01 11:24:35 +0800

    adjust port 9090

--- src/main.js
+++ src/main.js
@@ -9,55 +9,55 @@ Vue.http.options.emulateJSON = true;
 Vue.http.options.credentials = true;
 
 const router = new VueRouter({
-    hashbang: true,
-    history: false,
-    saveScrollPosition: true,
-    suppressTransitionError: true
+  hashbang: true,
+  history: false,
+  saveScrollPosition: true,
+  suppressTransitionError: true
 })
 routerconfig(router)
 
 localStorage.removeItem("indexData")
 
 let Init = () => {
-    var App = Vue.extend({});
-    router.start(App, 'body');
+  var App = Vue.extend({});
+  router.start(App, 'body');
 };
 
 if (process.env.NODE_ENV === 'production') {
-    init(() => {
-        //var sdk = SDK();
-        global.HOST = location.origin + "/publicapp";
-        global.IMGHOST = location.origin;
-        if (BH_MIXIN_SDK.bh) {
-            BH_MOBILE_SDK.UI.toggleNavBar(true);
-        }
-        Init()
-    }, {
-        wx: {
-            debug: false,
-            url: 'http://res.wisedu.com:8888/checkSign',
-            uploadImgsToEmapUrl: global.IMGHOST + '/uploadWxImgsToEmap',
-            emapPrefixPath: global.HOST
-        }
-    })
+  init(() => {
+    //var sdk = SDK();
+    global.HOST = location.origin + "/publicapp";
+    global.IMGHOST = location.origin;
+    if (BH_MIXIN_SDK.bh) {
+      BH_MOBILE_SDK.UI.toggleNavBar(true);
+    }
+    Init()
+  }, {
+    wx: {
+      debug: false,
+      url: 'http://res.wisedu.com:9090/checkSign',
+      uploadImgsToEmapUrl: global.IMGHOST + '/uploadWxImgsToEmap',
+      emapPrefixPath: global.HOST
+    }
+  })
 } else {
-    global.HOST = 'http://amptest.wisedu.com/publicapp'
-    global.IMGHOST = 'http://amptest.wisedu.com'
-        // Init();
-    init(() => {
-        //var sdk = SDK();
-        //global.HOST = 'http://amptest.wisedu.com/publicapp'
-        //global.IMGHOST = 'http://amptest.wisedu.com'
-        if (BH_MIXIN_SDK.bh) {
-            BH_MOBILE_SDK.UI.toggleNavBar(true);
-        }
-        Init()
-    }, {
-        wx: {
-            debug: false,
-            url: 'http://res.wisedu.com:8888/checkSign',
-            uploadImgsToEmapUrl: global.IMGHOST + '/uploadWxImgsToEmap',
-            emapPrefixPath: global.HOST
-        }
-    })
+  global.HOST = 'http://amptest.wisedu.com/publicapp'
+  global.IMGHOST = 'http://amptest.wisedu.com'
+    // Init();
+  init(() => {
+    //var sdk = SDK();
+    //global.HOST = 'http://amptest.wisedu.com/publicapp'
+    //global.IMGHOST = 'http://amptest.wisedu.com'
+    if (BH_MIXIN_SDK.bh) {
+      BH_MOBILE_SDK.UI.toggleNavBar(true);
+    }
+    Init()
+  }, {
+    wx: {
+      debug: false,
+      url: 'http://res.wisedu.com:9090/checkSign',
+      uploadImgsToEmapUrl: global.IMGHOST + '/uploadWxImgsToEmap',
+      emapPrefixPath: global.HOST
+    }
+  })
 }
